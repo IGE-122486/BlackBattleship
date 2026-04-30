2a839219eec86ad8a82fca62f28aead1987d06d5
Bartolomeu Costa- IGE-123779 <bartolomeu2004@gmail.com>
2026-04-30 14:58:44 +0100

    Melhora testes iniciais do projeto piloto

--- src/test/java/pt/iscte/blackbattleship/MainPageTest.java
+++ src/test/java/pt/iscte/blackbattleship/MainPageTest.java
@@ -1,6 +1,7 @@
 package pt.iscte.blackbattleship;
 
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -10,15 +11,27 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 public class MainPageTest {
 
     private WebDriver driver;
+    private MainPage mainPage;
+
+    @BeforeEach
+    void setUp() {
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+
+        mainPage = new MainPage(driver);
+        mainPage.open();
+    }
 
     @Test
     void testOpenMainPage() {
-        driver = new ChromeDriver();
-
-        MainPage mainPage = new MainPage(driver);
-        mainPage.open();
-
         assertTrue(mainPage.getTitle().contains("JetBrains"));
+    }
+
+    @Test
+    void testSearchButtonOpensSearchInput() {
+        mainPage.clickSearch();
+
+        assertTrue(mainPage.isSearchInputVisible());
     }
 
     @AfterEach
