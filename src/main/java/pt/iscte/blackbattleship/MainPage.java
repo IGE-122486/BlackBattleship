bca973aaffad40a86d1a024c646787f18d1507fa
Miguel <mike.almeida.paiva@gmail.com>
2026-04-30 16:23:52 +0100

    Melhoria das classes MainPage e MainPageTest

--- src/main/java/pt/iscte/blackbattleship/MainPage.java
+++ src/main/java/pt/iscte/blackbattleship/MainPage.java
@@ -2,6 +2,9 @@ package pt.iscte.blackbattleship;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
 
 public class MainPage {
 
@@ -20,6 +23,18 @@ public class MainPage {
     }
 
     public void clickSearch() {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+
+        // espera até encontrar o botão e só depois clica
+        wait.until(d -> d.findElement(By.cssSelector("button[aria-label='Search']")));
         driver.findElement(By.cssSelector("button[aria-label='Search']")).click();
+    }
+
+    public boolean isSearchInputVisible() {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+
+        return wait.until(d ->
+                d.findElement(By.cssSelector("input[type='search']")).isDisplayed()
+        );
     }
 }
--- src/test/java/pt/iscte/blackbattleship/MainPageTest.java
+++ src/test/java/pt/iscte/blackbattleship/MainPageTest.java
@@ -1,10 +1,12 @@
 package pt.iscte.blackbattleship;
 
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
+
 import java.time.Duration;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
@@ -12,17 +14,24 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 public class MainPageTest {
 
     private WebDriver driver;
+    private MainPage mainPage;
+
+    @BeforeEach
+    void setUp() {
+        driver = new ChromeDriver();
+        driver.manage().window().maximize(); // 👈 melhoria
+
+        mainPage = new MainPage(driver);
+        mainPage.open();
+    }
 
     @Test
-    void testOpenMainPage() {
-        driver = new ChromeDriver();
-
-        MainPage mainPage = new MainPage(driver);
-        mainPage.open();
+    void testOpenMainPage() throws InterruptedException {
 
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
-
         wait.until(d -> d.getTitle().contains("JetBrains"));
+
+        Thread.sleep(5000); // 5s p ver o site
 
         assertTrue(mainPage.getTitle().contains("JetBrains"));
     }
